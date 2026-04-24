6d26e57666d0af52205a50d8445fd171d276d3c2
Eaoumoon Haque <22-47526-2@student.aiub.edu>
2026-04-24 10:35:59 +0600

    Goal based basic agent created

---
+++ Backend/ai-agent.js
@@ -0,0 +1,37 @@
+const OpenAI = require("openai");
+require("dotenv").config();
+const openai = new OpenAI({
+  baseURL: "https://openrouter.ai/api/v1",
+  apiKey: process.env.OPENROUTER_API_KEY,
+});
+
+async function main() {
+  const completion = await openai.chat.completions.create({
+    model: "baidu/qianfan-ocr-fast:free",
+    //google/gemma-3-12b-it:free
+    messages: [{ role: "user", content: "what is agentic AI?" }],
+  });
+
+  console.log(completion.choices[0].message.content);
+}
+//main();
+
+const analyzeGoal = async () => {
+  const goalText = "Learn Python";
+  const durationDays = "7";
+
+  const propmt = `I want to ${goalText} within ${durationDays}, crate a clear structure for me`;
+  try {
+    const completion = await openai.chat.completions.create({
+      model: "baidu/qianfan-ocr-fast:free",
+      //google/gemma-3-12b-it:free
+      messages: [{ role: "user", content: propmt }],
+    });
+
+    console.log(completion.choices[0].message.content);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+analyzeGoal();
